52627fdf6a821a63af358e9177cb36f932955b66
Miguel Ángel Reyes Moreno <miguelreyesmoreno@hotmail.com>
2020-12-16 10:55:23 -0600

    Enlaces agregados a home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,17 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <nav class="nav justify-content-center">
+                        <a href="alumnos" class="nav-link">Alumnos</a>
+                        <a href="carreras" class="nav-link">Carreras</a>
+                        <a href="departamentos" class="nav-link">Departamentos</a>
+                        <a href="grupos" class="nav-link">Grupos</a>
+                        <a href="horarios" class="nav-link">Horarios</a>
+                        <a href="materias" class="nav-link">Materias</a>
+                        <a href="periodos" class="nav-link">Periodos</a>
+                        <a href="personales" class="nav-link">Personales</a>
+                        <a href="reticulas" class="nav-link">Retículas</a>
+                    </nav>
                 </div>
             </div>
         </div>
